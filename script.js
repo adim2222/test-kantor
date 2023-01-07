03b4e2fd48271a68a04cc1dbc5bd1e2432c0e599
Adam Nowak <nowak.adam2222@gmail.com>
2023-01-07 18:10:21 +0100

    Javascript refactoring

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-let amount = document.querySelector(".amount");
-let value = document.querySelector(".value");
-let display = document.querySelector(".display");
-let form = document.querySelector(".form");
+{
+    const amount = document.querySelector(".amount");
+    const value = document.querySelector(".value");
+    const display = document.querySelector(".display");
+    const form = document.querySelector(".form");
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    
-    let rawResult = amount.value * value.value;
-    let result = rawResult.toFixed(2);
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        let rawResult = amount.value * value.value;
+        let result = rawResult.toFixed(2);
 
-    display.innerHTML = `${result}PLN`;
-});
+        display.innerHTML = `${result}PLN`;
+    }
+
+    form.addEventListener("submit", onFormSubmit);
+}
